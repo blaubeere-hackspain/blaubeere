4c37d435e126820cead8dc08fc691369b41f8301
Saul Garcia <saul@livylabs.xyz>
2026-09-19 19:55:18 +0200

    style(ui): refine typography and responsive financial components

--- apps/app/app/layout.tsx
+++ apps/app/app/layout.tsx
@@ -1,7 +1,4 @@
 import "@fontsource-variable/inter";
-import "@fontsource/ibm-plex-sans/400.css";
-import "@fontsource/ibm-plex-sans/500.css";
-import "@fontsource/ibm-plex-sans/600.css";
 import "@fontsource/ibm-plex-mono/400.css";
 import type { Metadata } from "next";
 import { appOrigin, pageMetadata } from "../../metadata";
