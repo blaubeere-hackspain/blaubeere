3bb0cca2375b59cf6f10aa8963e68dff714bbb2b
Saul Garcia <saul@livylabs.xyz>
2026-09-19 14:13:08 +0200

    fix(app): preserve the existing chart font weight

--- apps/app/app/layout.tsx
+++ apps/app/app/layout.tsx
@@ -1,4 +1,5 @@
 import "@fontsource-variable/inter";
+import "@fontsource/ibm-plex-sans/400.css";
 import "@fontsource/ibm-plex-sans/500.css";
 import "@fontsource/ibm-plex-sans/600.css";
 import "@fontsource/ibm-plex-mono/400.css";
